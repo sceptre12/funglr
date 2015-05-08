5ab27d7a8a53fad9f173b0ed45f0446ed67301c4
Xavier <xthom001@gmail.com>
2015-05-08 01:24:19 -0600

    removed bower components

--- app/auth/js/authService.js
+++ app/auth/js/authService.js
@@ -1,30 +1,50 @@
-(function (window){
-	var angular = window.angular;
-	angular.module('funglr.auth')
-	.factory('AuthFactory',['FUNGLR_DB', function(FUNGLR_DB){
-		var ref = new Firebase(FUNGLR_DB);
-		// returns the if the user is authenticated or not
-		// in the case they aren't authenticated it returns null
-		this.user = ref.getAuth();
-		var saveNewUser = function(userObj){
-			ref.child('users').child(userObj.id).set(userObj);
-		};
-		this.isLoggedIn = function(){
-			return !!ref.getAuth();
-		};
+(function(window) {
+    var angular = window.angular;
+    angular.module('funglr.auth')
+        .factory('AuthFactory', ['FUNGLR_DB', function(FUNGLR_DB) {
+            var ref = new Firebase(FUNGLR_DB);
+            // returns the if the user is authenticated or not
+            // in the case they aren't authenticated it returns null
+            this.user = ref.getAuth();
+            var saveNewUser = function(userObj) {
+                ref.child('users').child(userObj.id).set(userObj);
+            };
+            this.isLoggedIn = function() {
+                return !!ref.getAuth();
+            };
 
-		this.loginWithPw= function(usrObj, callback, callbackOnRegister){
-			ref.authWithPassword(userObj, function(err,authData){
-				if(err){
-					console.log('error');
-				}else{
-					authData.email = userObj.email;
-					this.user = authData;
-					cb(authData);
-					callbackOnRegister && callbackOnRegister(true);
-				}
-			}.bind(this));
-		}
+            this.loginWithPw = function(usrObj, callback, callbackOnRegister) {
+                ref.authWithPassword(userObj, function(err, authData) {
+                    if (err) {
+                        console.log('error');
+                    } else {
+                        authData.email = userObj.email;
+                        this.user = authData;
+                        cb(authData);
+                        callbackOnRegister && callbackOnRegister(true);
+                    }
+                }.bind(this));
+            };
 
-	}]);
+            this.createUser = function(userName, callback) {
+                ref.createUser(userName, function(error, userData) {
+                    if (error) {
+                        switch (error.code) {
+                            case "EMAIL_TAKEN":
+                                console.log("The new user account cannot be created because the email is already in use.");
+                                break;
+                            case "INVALID_EMAIL":
+                                console.log("The specified email is not a valid email.");
+                                break;
+                            default:
+                                console.log("Error creating user:", error);
+                        }
+                    } else {
+                        console.log("Successfully created user account with uid:", userData.uid);
+                    }
+                })
+            }
+
+
+        }]);
 }(window));
